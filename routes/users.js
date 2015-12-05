a2bf08c0d3cd5a32dfb6ff5b2843372c6ffbea66
Jasen Pan <jasenpan@gmail.com>
2015-12-05 19:54:13 +1100

    add routes

--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,24 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+router.get('/reg', function(req, res, next) {
+  res.render('users/reg', { title: 'Express' });
+});
+
+router.post('/reg', function(req, res, next) {
+  res.send('注册');
+});
+
+router.get('/login', function(req, res, next) {
+  res.render('users/login', { title: 'Express' });
+});
+
+router.post('/login', function(req, res, next) {
+  res.send('登录');
+});
+
+router.get('/logout', function(req, res, next) {
+  res.render('users/logout', { title: 'Express' });
+});
+
 module.exports = router;
