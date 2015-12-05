d2d377367435d5c4ef4823d18779f9ac27cfae46
Jasen Pan <jasenpan@gmail.com>
2015-12-05 21:30:35 +1100

    add routes

--- routes/users.js
+++ routes/users.js
@@ -2,28 +2,46 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
-
 router.get('/reg', function(req, res, next) {
-  res.render('users/reg', { title: 'Express' });
+  res.render('users/reg',{});
 });
 
 router.post('/reg', function(req, res, next) {
-  res.send('注册');
+  var user =  req.body;//读取用户提交过来的注册表单
+  new Model('User')(user).save(function(err,user){
+    if(err){
+      res.redirect('/users/reg');
+    }else{
+      res.redirect('/users/login');
+    }
+  });
+
 });
 
 router.get('/login', function(req, res, next) {
-  res.render('users/login', { title: 'Express' });
+  res.render('users/login',{});
 });
 
 router.post('/login', function(req, res, next) {
-  res.send('登录');
+  var login = req.body;
+  //console.log(req.body);
+  Model('User').find({username:req.body.username, password:req.body.password}, function (err, doc){
+   if(err){
+     console.log(err)
+     res.redirect('/users/login');
+   }else{
+     console.log(doc);
+     if(doc.length>0){
+       res.redirect('/');
+     }else{
+       res.redirect('/users/login');
+     }
+   }
+  });
 });
 
 router.get('/logout', function(req, res, next) {
-  res.render('users/logout', { title: 'Express' });
+  res.send('退出');
 });
 
 module.exports = router;
